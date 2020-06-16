da96772c0d4534a4d2d4d104a154ceb43a2d8fac
Jeramie Andrews <drewsj00@gmail.com>
2020-06-16 13:31:28 -0500

    exercise 1 complete

--- oop1-construct.js
+++ oop1-construct.js
@@ -1,106 +1,183 @@
-//1. Create a Class called WhyClass.  This class should have one property, and two methods.  THe property is called "purpose", and stores a string.  This string should clearly explain why classes are used in Object Oriented Programming.  The first method is called explain().  This method should print the contents stored in the purpose 'field' of the class.  The second method is called pieces().  This 2nd method should print out the various parts that are essential to building a class.  Instantiate the class, and invoke the method so the results are showing in the console once this file is run.
+//1. Create a Class called WhyClass.  This class should have one property, and two methods.  THe property is called "purpose", and stores a string.
+//This string should clearly explain why classes are used in Object Oriented Programming.  The first method is called explain().
+//This method should print the contents stored in the purpose 'field' of the class.  The second method is called pieces().
+//This 2nd method should print out the various parts that are essential to building a class.
+//Instantiate the class, and invoke the method so the results are showing in the console once this file is run.
 
 //your code here...
 
+class WhyClass {
+  constructor(purpose, parts) {
+    this.purpose = purpose;
+    this.parts = parts;
+  }
 
+  explain() {
+    return this.purpose;
+  }
 
-/*2. You are exploring the rainforests of the Amazon.  You have observed many different types of wildlife on your excursion.  To jog your memory, some different types of wildlife that you observed are found in the following URL : https://www.ietravel.com/blog/amazon-rainforest-animals-beginners-guide-21-species.  Feel free to use other resources online if you wish.
+  pieces() {
+    return this.parts;
+  }
+}
 
-Create a class that can categorize at least 6 of the creatures that you recall observing the Amazon.  Each creature should have 3 unique characteristics, and 2 unique actions that it can do. */
+const reason = new WhyClass(
+  'In object-oriented programming, a class is an extensible program-code-template for creating objects, providing initial values for state, and implementations of behavior',
+  'constructor, properties, and methods'
+);
 
+console.log(reason.explain());
+
+console.log(reason.pieces());
+
+/*2. You are exploring the rainforests of the Amazon.  You have observed many different types of wildlife on your excursion.  
+To jog your memory, some different types of wildlife that you observed are found in the 
+following URL : https://www.ietravel.com/blog/amazon-rainforest-animals-beginners-guide-21-species.  Feel free to use other resources online if you wish.
+
+Create a class that can categorize at least 6 of the creatures that you recall observing the Amazon.  
+Each creature should have 3 unique characteristics, and 2 unique actions that it can do. */
 
 //your code here...
 
+class Animal {
+  constructor(name, type, color, attitude, action) {
+    this.name = name;
+    this.type = type;
+    this.color = color;
+    this.attitude = attitude;
+    this.action = action;
+  }
+}
 
+let animal1 = new Animal('River Dolphin', 'mammal', 'pink', 'friendly', [
+  'probing',
+  'sonar',
+]);
 
+console.log(animal1);
+
+let animal2 = new Animal('Capybara', 'rodent', 'brown', 'shy', [
+  'swim',
+  'dive',
+]);
+
+console.log(animal2);
+
+let animal3 = new Animal(
+  'Jaguar',
+  'mammal',
+  ['yellow', 'black', 'white', 'spotted'],
+  'agressive',
+  ['swims', 'territorial']
+);
+
+console.log(animal3);
+
+let animal4 = new Animal(
+  'Toco Toucan',
+  'bird',
+  ['black', 'white', 'yellow'],
+  'friendly',
+  ['sociable', 'hunter']
+);
+
+console.log(animal4);
+
+let animal5 = new Animal('Black Caiman', 'reptile', 'green', 'agressive', [
+  'hunting',
+  'swimming',
+]);
+
+console.log(animal5);
+
+let animal6 = new Animal(
+  'South American River Turtle',
+  'reptile',
+  'green',
+  'shy',
+  ['swimming', 'grow up to 200lbs']
+);
+
+console.log(animal6);
 
 /*3. Convert the following Object Literals into a general class called Shape, and instantiate the class to generate the object literals
-*/
-
+ */
 
 class Shape {
-    //your code here...
+  constructor(name, sides, base, height) {
+    this.name = name;
+    this.sides = sides;
+    this.base = base;
+    this.height = height;
+    this.area = () => console.log(`${this.base * this.height}`);
+    this.perimeter = () =>
+      console.log(`${this.base + this.sides[1] + this.sides[2]}`);
+  }
 }
 
+const shape1 = new Shape('Triangle', [4, 7, 7], 4, 6.7);
 
-const triangle = {
-    name : "triangle",
-    sides : [4,7,7], //lengths of each side
-    base : null,
-    height : null, //determine this height using basic geometry.  You may need to research this one.
-    calcArea : function() {
-        console.log(`${this.name}'s area is calculated to be : ${.5 * this.base * this.height}`);
-    },
-    calcPerimeter : function() {
-        console.log(`${this.name}'s perimeter is calculated to be : ${(this.base) + (this.sides[1] + this.sides[2]) }`)
-    }
+console.log(shape1);
+shape1.area();
+shape1.perimeter();
+
+class Shape2 {
+  constructor(name, sides, length, width) {
+    this.name = name;
+    this.sides = sides;
+    this.length = length;
+    this.width = width;
+    this.area = () => console.log(`${this.length * this.width}`);
+    this.perimeter = () => console.log(`${2 * this.length + 2 * this.width}`);
+  }
 }
 
-triangle.base = triangle.sides[0];  //base is the 1st side in the triangle.sides array.
-triangle.height = 6.71; //determine the height using basic geometry.  How do you calculate the height of a triangle with 2 equal sides?  If you use the formula to caluclate the height, this is a bonus.  If you hard code the correct value here, that will be sufficient, but no bonus :(
+const rect = new Shape2('Rectangle', 4, 2, 5);
 
-console.log(triangle);
-triangle.calcArea();
-triangle.calcPerimeter();
+console.log(rect);
+rect.area();
+rect.perimeter();
 
-const rectangle = {
-    name : "rectangle",
-    sides : 4,
-    length : 2,
-    width : 5,
-    calcArea : function() {
-        console.log(`${this.name}'s area is calculated to be : ${this.length * this.width}`);
-    },
-    calcPerimeter : function() {
-        console.log(`${this.name}'s perimeter is calculated to be : ${(2 * this.length) + (2 * this.width) }`)
-    }
+class Shape3 {
+  constructor(name, sides, radius) {
+    this.name = name;
+    this.sides = sides;
+    this.radius = radius;
+    this.area = () => console.log(`${(Math.PI * this.radius ** 2).toFixed(2)}`);
+    this.circumference = () =>
+      console.log(`${(2 * Math.PI * this.radius).toFixed(2)}`);
+  }
 }
 
-console.log(rectangle);
-rectangle.calcArea();
-rectangle.calcPerimeter();
+const cir = new Shape3('Circle', 1, 5);
 
-const circle = {
-    name : "circle",
-    sides : 1,
-    radius : 5,
-    calcArea : function() {
-        console.log(`${this.name}'s area is calculated to be : ${(Math.PI * this.radius**2).toFixed(2)}`);
-    },
-    calcCircumference : function() {
-        console.log(`${this.name}'s circumference is calculated to be : ${(2 * Math.PI * this.radius).toFixed(2)}`)
-    }
-}
-
-console.log(circle);
-circle.calcCircumference();
-circle.calcArea();
-
+console.log(cir);
+cir.area();
+cir.circumference();
 
 /*********************************************** 
 Bonus Exercises:
 
-//4. Create three more instances of the Shape class for a pentagon, hexagon, and an octagon.  Add in  comments explaining the formulas for calculating the perimeter and the areas of these 3 shapes.  Also, as noted above, if you use the formula to calculate the height of the triangle instead of hard coding it, you will also obtain the bonus for this exercise.  The dimensions for the triangle are already provided for you in the triangle object literal.
+//4. Create three more instances of the Shape class for a pentagon, hexagon, and an octagon.  
+//Add in  comments explaining the formulas for calculating the perimeter and the areas of these 3 shapes.  
+//Also, as noted above, if you use the formula to calculate the height of the triangle instead of hard coding it, 
+//you will also obtain the bonus for this exercise.  The dimensions for the triangle are already provided for you in the triangle object literal.
 
 
-//5. Below is a class Earth.  It is instantiated with a earth 'instance'.  Because there is only 1 known earth, we don't want multiple instances of earth.  Modify the class so the properties are usuable WITHOUT instantiating the class.
+//5. Below is a class Earth.  It is instantiated with a earth 'instance'.  Because there is only 1 known earth, we don't want multiple instances of earth. 
+Modify the class so the properties are usuable WITHOUT instantiating the class.
 
 */
 
-class Earth {
-    name;
-    planetNum; //distance from the sun.  1-mercury, 2-venus, 3-earth, ...
+class Planet {
+  name;
+  planetNum; //distance from the sun.  1-mercury, 2-venus, 3-earth, ...
 
-    constructor(name,num) {
-        this.name = name;
-        this.planetNum = num;
-    }
-
+  constructor(name, num) {
+    this.name = name;
+    this.planetNum = num;
+  }
 }
 
-const earth = new Earth('earth',3);
+const earth = new Planet('earth', 3);
 console.log(earth);
-
-
-
-
